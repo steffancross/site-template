b9d38ad82ca7d52f3f4c5eb61b6a133cd5f7b5b1
steffancross <steffancross@berkeley.edu>
2026-05-18 13:36:34 -0700

    intial a1 pass

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  trailingSlash: false,
+};
+
+export default nextConfig;
